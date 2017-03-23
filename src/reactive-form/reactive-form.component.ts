86ce325c48faa3e0693287c84d8f289a01a764b7
Jean Baptiste Marchetti <marchetti.jb@gmail.com>
2017-03-23 14:06:35 +0100

    chore: bump version number

--- src/reactive-form/reactive-form.component.ts
+++ src/reactive-form/reactive-form.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'tw-reactive-form',
   template: `
-<form [formGroup]="form" *ngIf='fieldsInit'>
+<form [formGroup]="form">
   <div class="row">
     <div class="form-group col-{{field.col || 'sm-6'}}" [ngClass]="field.class" *ngFor="let field of fields">
       <label for="{{field.id}}">{{field.label | translate}} <small *ngIf='field.info'>{{field.info | translate}}</small></label>
@@ -35,7 +35,6 @@ export class ReactiveFormComponent implements OnInit {
   public submitted: boolean; // keep track on whether form is submitted
   public events: any[] = []; // use later to display form changes
 
-  public fieldsInit: boolean = false
   constructor() {
     this.afterInit = new EventEmitter<boolean>(true)
   }
@@ -52,7 +51,6 @@ export class ReactiveFormComponent implements OnInit {
       this.form.addControl(field.id, control)
     })
 
-    this.fieldsInit = true
     this.afterInit.next(true)
   }
 
