cfefc211604fd798d6b4b7616bdba38a648c18ce
Jean Baptiste Marchetti <marchetti.jb@gmail.com>
2017-03-27 13:48:08 +0200

    chore: bump version number

--- src/reactive-form/treeview-txt/treeview-txt.component.ts
+++ src/reactive-form/treeview-txt/treeview-txt.component.ts
@@ -9,7 +9,8 @@ import { FormField } from '../form-field.model'
   selector: 'tw-treeview-txt',
   template: `
   <div [formGroup]='group'>
-   <leo-dropdown-treeview id="treeview_{{field.id}}" [config]="selectConfig" [items]="field.options" (selectedChange)="updateModel($event)"></leo-dropdown-treeview>
+   <leo-treeview *ngIf='field.type==="treeview-txt"' id="treeview_{{field.id}}" [config]="selectConfig" [items]="field.options" (selectedChange)="updateModel($event)"></leo-treeview>
+   <leo-dropdown-treeview *ngIf='field.type==="dropdown-treeview-txt"' id="treeview_{{field.id}}" [config]="selectConfig" [items]="field.options" (selectedChange)="updateModel($event)"></leo-dropdown-treeview>
     <input [formControlName]='field.id' name='{{field.id}}' type='hidden' class='form-control' [(ngModel)]="request[field.id]"/>
 </div>
   `
