2fd15a5f4717f5260b355eb11f47f64b661d80a1
Jean Baptiste Marchetti <marchetti.jb@gmail.com>
2017-04-06 10:27:09 +0200

    chore: bump version number

--- src/angularFormsUtils.module.ts
+++ src/angularFormsUtils.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { NguiDatetimePickerModule } from '@ngui/datetime-picker';
 import { DatetimePickerComponent } from './reactive-form/datetime-picker/datetime-picker.component';
 import { DatePickerComponent } from './reactive-form/date-picker/date-picker.component';
 import { SelectComponent } from './reactive-form/select/select.component';
@@ -51,7 +50,6 @@ let decExp: any[] =
     ReactiveFormsModule,
     CommonModule,
     TranslateModule,
-    NguiDatetimePickerModule,
     DateTimePickerModule,
     FormsModule,
     DropdownTreeviewModule
